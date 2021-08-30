60c9c78e807a0bf500223e6a314657a7468ae0e6
gosseman <tpwls0301@nate.com>
2021-08-30 23:50:29 +0900

    django

--- api_tool.py
+++ api_tool.py
@@ -132,7 +132,7 @@ def call_match(gameId):
     elif res.status_code == 415:
         print("Match Error) : 415 (Unsupported Media Type) This error indicates that the server is refusing to service the request because the body of the request is in a format that is not supported.")
     elif res.status_code == 429:
-        print("vError) : 429 (Rate Limit Exceeded) This error indicates that the application has exhausted its maximum number of allotted API calls allowed for a given duration.")
+        print("Match Error) : 429 (Rate Limit Exceeded) This error indicates that the application has exhausted its maximum number of allotted API calls allowed for a given duration.")
     elif res.status_code == 500:
         print("Match Error) : 500 (Internal Server Error) This error indicates an unexpected condition or exception which prevented the server from fulfilling an API request.")
     elif res.status_code == 503:
@@ -163,6 +163,31 @@ def call_match_v5(gameId):
         print("Match_v5 Error) : 500 (Internal Server Error) This error indicates an unexpected condition or exception which prevented the server from fulfilling an API request.")
     elif res.status_code == 503:
         print("Match_v5 Error) : 503 (Service Unavailable) This error indicates the server is currently unavailable to handle requests because of an unknown reason. The Service Unavailable response implies a temporary condition which will be alleviated after some delay.")
+    else:
+        print("Unknown Error")
+    return 0
+
+def call_match_timeline(gameId):
+    url = "https://kr.api.riotgames.com/lol/match/v4/timelines/by-match/" + gameId
+    res = requests.get(url, headers={"X-RIOT-Token":api_key})
+    if res.status_code == 200:
+        return json.loads(res.text)
+    elif res.status_code == 400:
+        print("Match Error) 400 (Bad Request) : This error indicates that there is a syntax error in the request and the request has therefore been denied.")
+    elif res.status_code == 401:
+        print("Match Error) : 401 (Unauthorized) This error indicates that the request being made did not contain the necessary authentication credentials (e.g., an API key) and therefore the client was denied access.")
+    elif res.status_code == 403:
+        print("Match Error) : 403 (Forbidden) This error indicates that the server understood the request but refuses to authorize it.")
+    elif res.status_code == 404:
+        print("Match Error) : 404 (Not Found) This error indicates that the server has not found a match for the API request being made.")
+    elif res.status_code == 415:
+        print("Match Error) : 415 (Unsupported Media Type) This error indicates that the server is refusing to service the request because the body of the request is in a format that is not supported.")
+    elif res.status_code == 429:
+        print("vError) : 429 (Rate Limit Exceeded) This error indicates that the application has exhausted its maximum number of allotted API calls allowed for a given duration.")
+    elif res.status_code == 500:
+        print("Match Error) : 500 (Internal Server Error) This error indicates an unexpected condition or exception which prevented the server from fulfilling an API request.")
+    elif res.status_code == 503:
+        print("Match Error) : 503 (Service Unavailable) This error indicates the server is currently unavailable to handle requests because of an unknown reason. The Service Unavailable response implies a temporary condition which will be alleviated after some delay.")
     else:
         print("Unknown Error")
     return 0
--- call.py
+++ call.py
@@ -7,7 +7,7 @@ api_key = "RGAPI-6a377f3a-7ddf-42fe-9f2c-bf1469161a10"
 summoner_json = api_tool.call_summoner()
 matchlist_json = api_tool.call_matchlist(summoner_json['accountId'])
 champ_json = api_tool.call_champ()
-
+print(summoner_json)
 with open('champ_engtokor.json', 'r',encoding='UTF8') as champ_engtokor_file:
     champEtoK_json = json.load(champ_engtokor_file)
 with open('game_type.json', 'r',encoding='UTF8') as game_type_file:
@@ -19,7 +19,7 @@ output_dict['match_log'] = []
 n=0
 i=0
 for matches in matchlist_json:
-    print(api_tool.call_match_v5(str(matches["gameId"])))
+
     if i==1:
         break;
     i = i + 1
@@ -31,16 +31,23 @@ for matches in matchlist_json:
                 game_type = gameType_json[str(matches['queue'])]
             else:
                 game_type = '기타모드'
-            #rint(game_type, '-', champEtoK_json[champ], '-', matches["role"], ":", matches["lane"])
+            print(game_type, '-', champEtoK_json[champ], '-', matches["role"], ":", matches["lane"])
             exp_json = api_tool.call_exp(summoner_json['id'],str(matches['champion']))
-            #print("숙련도 : ",exp_json['championPoints'])
+            print("숙련도 : ",exp_json['championPoints'])
             match_json = api_tool.call_match(str(matches["gameId"]))
-            print(match_json)
+            #print(match_json)
+            match_timeline_json = api_tool.call_match_timeline(str(matches["gameId"]))
+            print(match_timeline_json)
+            with open('output.json', 'w') as outfile:
+                json.dump(match_timeline_json, outfile, indent=4)
             # print(matchdetail_json.text)
-            #print("승패 : "+match_json['teams'][0]['win'])
+            print("승패 : "+match_json['teams'][0]['win'])
+
     n=n+1
     output_dict['match_log'].append({"match_index":n, "game_type":game_type, "champ": champ_name,"role" : matches["role"], "lane" : matches["lane"], "result":match_json['teams'][0]['win']})
 #print(output_dict)
+
+
 '''
 with open('output.json', 'w') as outfile:
     json.dump(output_dict, outfile, indent=4)
