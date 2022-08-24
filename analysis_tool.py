7e63ce503a854bd1f6a6111f796116a46a3ad1fe
psj <psj14207@snu.ac.kr>
2022-08-24 09:53:07 +0900

    0824

--- analysis_tool.py
+++ analysis_tool.py
@@ -64,21 +64,27 @@ def challenger_list():
         chal_list.append(i.get("summonerName"))
     f = open("./challenger_list", 'w')
     for i in chal_list:
-        f.write(i.encode('utf-8'))
+        f.write(i)
         f.write('\n')
     f.close()
     return chal_list
 
-# 해당 유저 최근 10 게임중 랭크에서 정글 플레이한 경우 동선 이미지 저장
-def jungle_move_map(summonerName, t, out_path):
+# 해당 유저 최근 n 게임중 랭크에서 정글 플레이한 경우 t회 동선 이미지 저장
+def jungle_move_map(summonerName, t, n, out_path):
     summoner_json = api_tool.call_summoner(summonerName)
     summonerName = summonerName.replace(" ", "").strip().lower()
     puuid = summoner_json.get("puuid")
     match_list = api_tool.call_matchlist(puuid,1,8)
     map_img = cv2.imread("./IMG/map/map.png", cv2.IMREAD_COLOR)
+
     if not os.path.isdir(out_path):
         os.makedirs(out_path)
     resized_map = cv2.resize(map_img, (1600,1600),cv2.INTER_LINEAR)
+    print(resized_map.shape)
+    for i in range(1600):
+        if resized_map[1300][i][0] != 0:
+            print(i)
+            break
     cv2.imwrite('./IMG/map/resized_map.png', resized_map)
     k = 0
     if puuid == None:
@@ -108,27 +114,29 @@ def jungle_move_map(summonerName, t, out_path):
                         print("y : "+str(y))
                         n_x = round(x/10)
                         n_y = round(y/10)
-                        cv2.line(map_img, (n_x, n_y), (n_x, n_y), (255, 0, 0), 20)
+                        cv2.line(map_img, (1600 - n_y, n_x), (1600 - n_y, n_x), (255, 0, 0), 20)
+                        cv2.putText(map_img, str(idx), (1600 - n_y, n_x), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 2, cv2.LINE_AA)
+
                         plt.xlim(0, 16000)
                         plt.ylim(0, 16000)
                         plt.plot(x, y, 'bo')
                         plt.text(x*1.01, y*1.01, idx, fontsize=12)
                         timestamp_num += 1
-                    print(map_img)
                     cv2.imwrite('./IMG/tmp/tmp.png', map_img)
                     cv2.imwrite(out_path + '/' + summonerName + str(k) + '.png', map_img)
-                    plt.show()
+                    #plt.show()
                     k += 1
-                        # 이 번호를 가지고 timeline_json 에서 timeframe별 좌표 구하면됨
+                    if k >= n:
+                        return
+                    # 이 번호를 가지고 timeline_json 에서 timeframe별 좌표 구하면됨
     return
-
 
 
 def main():
     #check_lane_n("omgabie" ,10)
 
     # 1. challenger list json file 생성
-    #chal_list = challenger_list()
+    chal_list = challenger_list()
 
     # 2. challenger position list
     '''
@@ -171,12 +179,12 @@ def main():
         ratio.append(pos_dict.get(i))
     print(ratio)
     colors = ['red', 'violet', 'purple', 'olivedrab', 'dodgerblue']
-    plt.pie(ratio, labels=labels, autopct = "%.1f%%", colors = colors)
+    plt.pie(ratio, labels=labels, autopct = ".1f%%", colors = colors)
     plt.show()
     '''
     #4. 챌린저 최근경기 동선 이미지 시각화
-    jungle_move_map("가쎄다", 5, "./IMG/tmp")
-    # 정글러 추출 및 챔피언*시간대 별 현위치 시각화
+    #jungle_move_map("가쎄다", 3, 3, "./IMG/tmp")
+   # 정글러 추출 및 챔피언*시간대 별 현위치 시각화
     return
 
 if __name__ == "__main__":
